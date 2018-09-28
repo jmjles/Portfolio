423a00ce9bff742a8174805ab778b33bfb8033c1
Jesus Morales Jr <jmjles@hotmail.com>
2018-09-27 22:28:56 -0700

    Added Play and Pause attr

--- JS/index.js
+++ JS/index.js
@@ -1,18 +1,13 @@
-$( "#pause" ).click(function() {
-  if ($( "#pause img" ).attr("src") == "./IMG/pause.png") {
-    $( "#pause img" ).attr("src", "./IMG/play.png");
-<<<<<<< HEAD
-    $( "#vid" ).get(0).play();
-  }
-  else {
-    $( "#pause img" ).attr("src", "./IMG/pause.png");
-    $( "#vid" ).get(0).pause();
-=======
-    $( "#backg" ).css("animation-play-state", "running");
-  }
-  else {
-    $( "#pause img" ).attr("src", "./IMG/pause.png");
-    $( "#backg" ).css("animation-play-state", "paused");
->>>>>>> 417d3ff7647db4510859ae7bc6b294d955e8694c
-  }
+$(document).ready(function() {
+  //Pause/Play funtion of Background
+  $( "#pause" ).click(function() {
+    if ($( "#pause img" ).attr("src") == "./IMG/pause.png") {
+      $( "#pause img" ).attr("src", "./IMG/play.png");
+      $( "#vid" ).get(0).play();
+    }
+    else {
+      $( "#pause img" ).attr("src", "./IMG/pause.png");
+      $( "#vid" ).get(0).pause();
+    }
+    });
 });
