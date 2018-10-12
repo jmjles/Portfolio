792769ca5c89c6ce26b280cff0e0951f1b025dbb
Jesus Morales Jr <jmjles@hotmail.com>
2018-10-12 04:45:12 -0700

    Added Clicker Game
    Routing And Template Established

---
+++ JS/clicker-game.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  //Pause/Play funtion of Background
+  $( "#pause" ).click(function() {
+    if ($( "#pause img" ).attr("src") == "../IMG/pause.png") {
+      $( "#pause img" ).attr("src", "../IMG/play.png");
+      $( "#vid" ).get(0).play();
+    }
+    else {
+      $( "#pause img" ).attr("src", "../IMG/pause.png");
+      $( "#vid" ).get(0).pause();
+    }
+    });
+});
