6ee6822cab7ac61ad047fa8f03c9796e7338f955
Jesus Morales Jr <jmjles@hotmail.com>
2018-10-29 18:21:38 -0700

    RM: Play/Pause Btn
    Started Development of clicker Game

--- JS/ko.js
+++ JS/ko.js
@@ -1,3 +1,4 @@
+var vm = new CharacterViewModel;
 
 function Character(name, title, count, url, codename){
   this.name = name;
@@ -5,15 +6,6 @@ function Character(name, title, count, url, codename){
   this.counter = count;
   this.url = url;
   this.code = codename;
-}
-function pumpkin() {
-  console.log("Test 1");
-}
-function ghoul() {
-  console.log("Test 2");
-}
-function ghost() {
-  console.log("Test 3");
 }
 function CharacterViewModel() {
 
@@ -30,10 +22,20 @@ function CharacterViewModel() {
 
     // Editable data
     this.characters = ko.observableArray([
-        pumpkin = new Character("Pumpkin", this.titled[0],1, "../IMG/pumpkin.png", "pumpkin" ),
-        ghoul = new Character("Ghoul", this.titled[2],1, "../IMG/ghoul.png", "ghoul" ),
-        ghost = new Character("Ghost", this.titled[2],1, "../IMG/ghost.png", "ghost" )
+        pumpkin = new Character("Pumpkin", this.titled[0],0, "../IMG/pumpkin.png", "pumpkin" ),
+        ghoul = new Character("Ghoul", this.titled[2],0, "../IMG/ghoul.png", "ghoul" ),
+        ghost = new Character("Ghost", this.titled[2],0, "../IMG/ghost.png", "ghost" )
     ]);
     this.test = "Tested";
+
+    this.pumpkin = function() {
+      console.log("Test 1");
+    }
+    this.ghoul = function() {
+      console.log("Test 2");
+    }
+    this.ghost = function() {
+      console.log("Test 3");
+    }
 }
-ko.applyBindings(new CharacterViewModel());
+ko.applyBindings(vm);
--- JS/location.js
+++ JS/location.js
@@ -79,17 +79,6 @@ function geocode() {
 }
 
 $(document).ready(function() {
-  //Pause/Play funtion of Background
-  $( "#pause" ).click(function() {
-    if ($( "#pause img" ).attr("src") == "../IMG/pause.png") {
-      $( "#pause img" ).attr("src", "../IMG/play.png");
-      $( "#vid" ).get(0).play();
-    }
-    else {
-      $( "#pause img" ).attr("src", "../IMG/pause.png");
-      $( "#vid" ).get(0).pause();
-    }
-    });
 
   var Locate = function() {
     //Search Value
